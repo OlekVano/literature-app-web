7c804cb4d33062d40b73a15b0b4e320198e5542b
OlekVano <olekvano@gmail.com>
2023-01-10 12:05:30 +0000

    Added support for image answer options

--- components/Question.tsx
+++ components/Question.tsx
@@ -1,3 +1,6 @@
+import Image from "next/image"
+import { isLink } from "../utils"
+
 interface State {
   question: string
   options: string[]
@@ -18,15 +21,24 @@ export default function Question({ state, validated, selectedOption, setSelected
   }
 
   if (state == null) return <></>
+
+  const isImageQuestion = isLink(state.options[0])
+
   return (
     <div>
       <h1 className='text-center font-bold text-4xl mb-6 text-white'>{state.question}</h1>
-      <div className='flex justify-center'>
-        <div className='flex flex-col'>
+      <div className='flex justify-center mb-6'>
+        <div className={`flex ${isImageQuestion ? 'flex-row' : 'flex-col'}`}>
         {state?.options.map((option, i) => {
           return <div className={`flex text-2xl ${validated && option == state.answer ? 'bg-green-500' : validated && option == selectedOption ? 'bg-red-500' : 'bg-transparent'}`} key={i}>
             <div className='p-1'><input className='h-6 w-6 orange' type='radio' disabled={validated} checked={selectedOption == '' ? false : undefined} onClick={() => handleInputClick(option)} name='option' id={stringToId(option)} /></div>
-            <label htmlFor={stringToId(option)}>{option}</label>
+            <label htmlFor={stringToId(option)} className='relative block'>
+              {
+                isImageQuestion ? <Image src={option} alt='Writer' width={200} height={200} className='pointer-events-none select-none p-3'></Image> :
+                <>{option}</>
+              }
+              
+            </label>
           </div>
         })}
         </div>
