b3040b5f9b5bd157663616246e7726308ff61a6b
OlekVano <olekvano@gmail.com>
2023-01-10 09:28:59 +0000

    Refactored Button1 code and made use of tailwind

--- components/Button1.tsx
+++ components/Button1.tsx
@@ -1,4 +1,3 @@
-import styles from './Button1.module.scss'
 import Link from 'next/link'
 import { MouseEventHandler } from 'react'
 
@@ -11,8 +10,12 @@ interface Props {
 
 }
 
-export default function Button1({ text, func, link, ariaLabel='', ariaDescribedby='' }: Props) {
-  if (typeof func !== 'undefined') return <div className={styles.main} role='button' aria-label={ariaLabel} aria-describedby={ariaDescribedby} onClick={func as MouseEventHandler<HTMLDivElement>}>{text}</div>
-  else if (typeof link !== 'undefined') return <Link href={link} className={styles.main}><div aria-label={ariaLabel} aria-describedby={ariaDescribedby}>{text}</div></Link>
-  else return <>Error</>
+// This is not the exported button!
+function Button({text, func, ariaLabel='', ariaDescribedby=''}: Omit<Props, 'link'>) {
+  return <div className='bg-orange cursor-pointer rounded-md shadow-lg text-center text-2xl font-medium p-3 m-1 duration-500 ease-in-out no-underline text-black hover:pl-9 hover:pr-9' role='button' aria-label={ariaLabel} aria-describedby={ariaDescribedby} onClick={func as MouseEventHandler<HTMLDivElement>}>{text}</div>
+}
+
+export default function Button1({ text, func=() => {}, link='', ariaLabel='', ariaDescribedby='' }: Props) {
+  if (link !== '') return <Link href={link}><Button text={text} func={func} ariaLabel={ariaLabel} ariaDescribedby={ariaDescribedby} /></Link>
+  return <Button text={text} func={func} ariaLabel={ariaLabel} ariaDescribedby={ariaDescribedby} />
 }
