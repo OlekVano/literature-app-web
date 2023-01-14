63df7164dacf28bf8dc57219a40afd0efca65ce9
OlekVano <olekvano@gmail.com>
2023-01-14 22:58:23 +0000

    Added isImage function to utils.ts

--- utils.ts
+++ utils.ts
@@ -1,3 +1,13 @@
 export function isLink(str: string): boolean {
     return (str.startsWith('https://') || str.startsWith('http://'))
 }
+
+export function isImage(str: string): boolean {
+    const imageExtensions = [
+        'jpg',
+        'jpeg',
+        'png',
+        'webp',
+    ]
+    return imageExtensions.some(e => str.endsWith(e))
+}
